2acc87927799e9b0645a1e1b7c511b446ca879e5
LujanSolo <lujansolo@proton.me>
2022-09-16 14:14:27 -0700

    put master boostrap stylings in html; set border box and FIVE DAY FORECAST header to appear on weather fetch only

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,18 @@
-//! global variables
+//* global variables
 let userInput = document.getElementById("user-input");
 let searchBtn = document.getElementById("search-btn");
 let apiKey = "32f1cece631ee89046fe3328471647a0";
 let fiveDayEl = document.getElementById("five-day");
+let fiveDayHeader = document.getElementById("fiveDayHeader");
 
-//! 5day forecast div variable
+//* 5day forecast div variable
 let fiveDay = document.getElementById("five-day");
 
-//! Search history  div variable and bucket array to hold user's past input
+//* Search history  div variable and bucket array to hold user's past input
 let searchHistEl = document.getElementById("search-hist");
 let searchHistoryBucket = [] //! had set it to a const, rather than a changeable variable. see ln 24
 
-//! FUNCTION to run at PAGELOAD, call at end of index.js
+//* FUNCTION to run at PAGELOAD, call at end of index.js
 function init() {
   let storedCities = JSON.parse(localStorage.getItem("Loc."));//!was passing in incorrect key name
   console.log(storedCities)
@@ -22,7 +23,7 @@ function init() {
   showHistory();
 };
 
-//! FUNCTION to get items from Search History Bucket (e.g. local storage) and populate searchHistEl's
+//* FUNCTION to get items from Search History Bucket (e.g. local storage) and populate searchHistEl's
 function showHistory() {
   searchHistEl.innerHTML = "";
   for (var i=0; i < searchHistoryBucket.length; i++) {
@@ -33,12 +34,12 @@ function showHistory() {
   }
 };
 
-//! FUNCTION to store User Entries in local storage
+//* FUNCTION to store User Entries in local storage
 function storeCities() {
   localStorage.setItem("Loc.", JSON.stringify(searchHistoryBucket))
 }
 
-//! FUNCTION to feed user's city into query and fetch geographic longitude and latitude.  Note: this API is convenient in that you can search by city name, but the response data is limited; hence the need for the fetchWeather METHOD (below)
+//* FUNCTION to feed user's city into query and fetch geographic longitude and latitude.  Note: this API is convenient in that you can search by city name, but the response data is limited; hence the need for the fetchWeather METHOD (below)
 function fetchCoords(city) {
   let userQueryUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
 
@@ -52,7 +53,7 @@ function fetchCoords(city) {
     fetchWeather(lat, lon);
   });
 
-  //! METHOD to feed previous API's City coordinates into "data-rich" API. Note: this is a workaround for the "data-rich" API requiring long. and lat. coordinates to search for unique weather locations.
+  //* METHOD to feed previous API's City coordinates into "data-rich" API. Note: this is a workaround for the "data-rich" API requiring long. and lat. coordinates to search for unique weather locations.
   function fetchWeather(lat, lon) {
     console.log("The coordinates are latitude:", lat, " and longitude:", lon);
   
@@ -63,25 +64,26 @@ function fetchCoords(city) {
     .then((data) => renderWeather(data));
   };
   
-  
+
   function renderWeather(weather) {
     console.log(weather);
   
     let results = document.getElementById("main-display");
+    results.setAttribute('class', "border border-2 border-dark");
     
 
-    //!  a UNIX TIMESTAMP CONVERTER to take unix value from API and convert it to a readable format
+    //*  a UNIX TIMESTAMP CONVERTER to take unix value from API and convert it to a readable format
     let unixTimestamp = weather.current.dt*1000;
     let day = (new Date(unixTimestamp).toDateString()); 
     
-    //! shortcut to weather array details
+    //* shortcut to weather array details
     let weatherDetails = weather.current.weather[0];
 
     if (results !== null) {
       results.innerHTML = "";
     }
 
-    //!items below to populate current weather in "results" div 
+    //* Items below to populate current weather in "results" div 
     let cityInput = document.createElement("h2");
     cityInput.innerHTML =  city + ' ' + "(" + day +")";
     results.append(cityInput);
@@ -106,7 +108,7 @@ function fetchCoords(city) {
     uvIndex.innerHTML = "UV Index: " + weather.current.uvi;
     results.append(uvIndex); //todo: Add color coded <SPAN> over uvIndex
 
-    //! FOR LOOP set to i=1, as we are looking for the next 5 days of weather for our cards.
+    //* FOR LOOP set to i=1, as we are looking for the next 5 days of weather for our cards.
     for (let i = 1; i < 6; i++) {
       let currentDay = weather.daily[i];
       makeFiveDay(currentDay);
@@ -115,10 +117,10 @@ function fetchCoords(city) {
 };
 // console.log(makeFiveDay());
 
-// //! FUNCTION to create FIVE DAY forecast cards. Attribute details pulled directly from bootstrap cards. 
+// //* FUNCTION to create FIVE DAY forecast cards. Attribute details pulled directly from bootstrap cards. 
 function makeFiveDay(day) {
   console.log(day);
-
+  fiveDayHeader.style.display = "";
   let date = (new Date(day.dt*1000).toDateString()); 
   console.log(date);
 
@@ -134,36 +136,37 @@ function makeFiveDay(day) {
   cardContainer.append(cardBody);
 
 
-  //! Card Title (our future day DATE)
+  //* Card Title (our future day DATE)
   let cardTitle = document.createElement("h6");
-  cardTitle.setAttribute('class', "card-title text-left");
+  cardTitle.setAttribute('class', "card-title");
   cardTitle.textContent = date;
   cardBody.append(cardTitle);
   
-  //! Descriptive weather Icon
+  //* Descriptive weather Icon
+  //todo GET ICON TO ALWAYS SET AT END OF TITLE(DATE) maybe pseudo? float?
   let cardIcon = document.createElement("img");
   cardIcon.setAttribute("src", ("http://openweathermap.org/img/wn/" + day.weather[0].icon + ".png"));
-  cardBody.append(cardIcon);
+  cardTitle.append(cardIcon);
 
-  //! Temperature readout:
+
+  //* Temperature readout:
   let cardTemp = document.createElement("p");
   cardTemp.setAttribute('class', "card-text");
-  cardTemp.textContent = "High temp.: " + day.temp.max;
+  cardTemp.textContent = "Temp: " + day.temp.max;
   console.log(day.temp.max);
   cardBody.append(cardTemp);
 
-  // //! Wind readout:
+  //* Wind readout:
   let cardWind = document.createElement("p");
   cardWind.setAttribute('class', "card-text");
-  cardWind.textContent = "Wind speed: " + day.wind_speed + "mph";
-  cardTemp.append(cardWind);
+  cardWind.textContent = "Wind: " + day.wind_speed + "mph";
+  cardBody.append(cardWind);
 
-  //! Humidity Readout
-
+  //* Humidity Readout
   let cardHumidity = document.createElement("p");
   cardHumidity.setAttribute('class', "card-text");
   cardHumidity.textContent = "Humidity: " + day.humidity + "%";
-  cardWind.append(cardHumidity);
+  cardBody.append(cardHumidity);
 
   return cardContainer;
   cardContainer.append(fiveDayEl)
@@ -171,7 +174,7 @@ function makeFiveDay(day) {
 
 init();
 
-//! THE ONE BUTTON TO RULE THEM ALL
+//* THE ONE BUTTON TO RULE THEM ALL
 searchBtn.addEventListener("click", function(event) {
   let userCity = userInput.value.trim();
   
@@ -179,7 +182,7 @@ searchBtn.addEventListener("click", function(event) {
     alert("Please enter a valid city/location"); //todo: change alert to MODAL? 
     return;
   };
-  //! push user's entry into global bucket (array)
+  //* push user's entry into global bucket (array)
   //TODO - create an IF CLAUSE to check entry against Search History
   searchHistoryBucket.push(userCity);
   userInput.value = "";
@@ -191,8 +194,7 @@ searchBtn.addEventListener("click", function(event) {
 });
 
 
-
-
+//todo BUILD COLOR CODING FUNCTION
 // //write getUVColor function00
 // //*COLOR CODING FOR 
 //   //*  -favorable
