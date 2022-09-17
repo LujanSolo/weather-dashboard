c3c7c72987e659d075d7764144c05dfa71105582
LujanSolo <lujansolo@proton.me>
2022-09-16 20:16:03 -0700

    added conditional statement to remove duplicate entries

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,96 +10,115 @@ let fiveDay = document.getElementById("five-day");
 
 //* Search history  div variable and bucket array to hold user's past input
 let searchHistEl = document.getElementById("search-hist");
-let searchHistoryBucket = [] 
+let searchHistoryBucket = [];
 
-//* FUNCTION to run at PAGELOAD, call at end of index.js
+//* FUNCTION to run at PAGELOAD, called at end of this file
 function init() {
   let storedCities = JSON.parse(localStorage.getItem("Loc."));
-  console.log(storedCities)
+  console.log(storedCities);
   // if local storage isn't empty, update bucket with stored entries
   if (storedCities !== null) {
-    searchHistoryBucket = storedCities; 
-  };
+    searchHistoryBucket = storedCities;
+  }
   showHistory();
-};
+}
 
 //* FUNCTION to get items from Search History Bucket (e.g. local storage) and populate searchHistEl's
 function showHistory() {
   searchHistEl.innerHTML = "";
-  for (var i=0; i < searchHistoryBucket.length; i++) {
-  let historyBtn = document.createElement("button");
-  historyBtn.setAttribute("class", "history-btn border-0 rounded w-100 bg-dark text-white p-1 m-1")
-  historyBtn.innerHTML = searchHistoryBucket [i];
-  searchHistEl.appendChild(historyBtn);
+  for (var i = 0; i < searchHistoryBucket.length; i++) {
+    let historyBtn = document.createElement("button");
+    historyBtn.setAttribute(
+      "class",
+      "history-btn border-0 rounded w-100 bg-dark text-white p-1 m-1"
+    );
+    historyBtn.innerHTML = searchHistoryBucket[i];
+    searchHistEl.appendChild(historyBtn);
   }
-};
+}
 
 //* FUNCTION to store User Entries in local storage
+
 function storeCities() {
-  localStorage.setItem("Loc.", JSON.stringify(searchHistoryBucket))
+  localStorage.setItem("Loc.", JSON.stringify(searchHistoryBucket));
 }
 
 //* FUNCTION to feed user's city into query and fetch geographic longitude and latitude.  Note: this API is convenient in that you can search by city name, but the response data is limited; hence the need for the fetchWeather METHOD (below)
 function fetchCoords(city) {
-  let userQueryUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
+  let userQueryUrl =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    city +
+    "&appid=" +
+    apiKey;
 
   fetch(userQueryUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (coords) {
-    let lat = coords[0].lat;
-    let lon = coords[0].lon;
-    fetchWeather(lat, lon);  //! ADD A CONDITIONAL HERE TO DEAL WITH INVALID INPUT?
-  });
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (coords) {
+      let lat = coords[0].lat;
+      let lon = coords[0].lon;
+      fetchWeather(lat, lon); //TODO ADD A CONDITIONAL HERE TO DEAL WITH INVALID INPUT/EMPTY FIELD/duplicates? USE MODAL
+    });
 
   //* METHOD to feed previous API's City coordinates into "data-rich" API. Note: this is a workaround for the "data-rich" API requiring long. and lat. coordinates to search for unique weather locations.
   function fetchWeather(lat, lon) {
     console.log("The coordinates are latitude:", lat, " and longitude:", lon);
-  
-    let url = "https://pro.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=" + apiKey;
-  
+
+    let url =
+      "https://pro.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&units=imperial&exclude=minutely,hourly&appid=" +
+      apiKey;
+
     fetch(url)
-    .then((response) => response.json())
-    .then((data) => renderWeather(data));
-  };
-  
+      .then((response) => response.json())
+      .then((data) => renderWeather(data));
+  }
 
   function renderWeather(weather) {
     console.log(weather);
-    
+
     let results = document.getElementById("main-display");
-    results.setAttribute('class', "border border-2 border-dark bg-primary text-light");
-    
+    results.setAttribute(
+      "class",
+      "border border-2 border-dark bg-primary text-light"
+    );
+    //todo INDENT TEXT IN THE DIV TO SEPARATE FROM BORDER
 
     //*  a UNIX TIMESTAMP CONVERTER to take unix value from API and convert it to a readable format
-    let unixTimestamp = weather.current.dt*1000;
-    let day = (new Date(unixTimestamp).toDateString()); 
-    
+    let unixTimestamp = weather.current.dt * 1000;
+    let day = new Date(unixTimestamp).toDateString();
+
     //* shortcut to weather array details
     let weatherDetails = weather.current.weather[0];
-    
-    //* Clears out current weather data if there is any
+
     if (results !== null) {
       results.innerHTML = "";
     }
+
     if (fiveDayEl !== null) {
-      fiveDayEl.innerHTML =""
+      fiveDayEl.innerHTML = "";
     }
 
-    //* Items below to populate current weather in "results" div 
+    //* Items below to populate current weather in "results" div
     let cityInput = document.createElement("h2");
-    cityInput.innerHTML =  city + ' ' + "(" + day +")";
+    cityInput.innerHTML = city + " " + "(" + day + ")";
     results.append(cityInput);
-  
+
     let weatherImg = document.createElement("img");
-    weatherImg.setAttribute("src", ("http://openweathermap.org/img/wn/" + weatherDetails.icon + ".png"));
+    weatherImg.setAttribute(
+      "src",
+      "http://openweathermap.org/img/wn/" + weatherDetails.icon + ".png"
+    );
     cityInput.appendChild(weatherImg);
-  
+
     let temp = document.createElement("p");
     temp.innerHTML = "Temp: " + weather.current.temp + " °F";
     results.append(temp);
-  
+
     let wind = document.createElement("p");
     wind.innerHTML = "Wind Speed: " + weather.current.wind_speed + " mph";
     results.append(wind);
@@ -107,91 +126,93 @@ function fetchCoords(city) {
     let humidity = document.createElement("p");
     humidity.innerHTML = "Humidity: " + weather.current.humidity + "%";
     results.append(humidity);
-  
+
     let uvIndex = document.createElement("p");
     uvIndex.innerHTML = "UV Index: " + weather.current.uvi;
-    results.append(uvIndex); //todo: Add color coded <SPAN> over uvIndex
+    results.append(uvIndex);
+    //todo: Add color coded <SPAN> over uvIndex
 
     //* FOR LOOP set to i=1, as we are looking for the next 5 days of weather for our cards.
     for (let i = 1; i < 6; i++) {
       let currentDay = weather.daily[i];
       makeFiveDay(currentDay);
-      }
-  };
-};
-// console.log(makeFiveDay());
+    }
+  }
+}
 
-// //* FUNCTION to create FIVE DAY forecast cards. Attribute details pulled directly from bootstrap cards. 
+// //* FUNCTION to create FIVE DAY forecast cards. Attribute details pulled directly from bootstrap cards.
 function makeFiveDay(day) {
   console.log(day);
   //* sets the "Five Day Forecast" header to display upon five day rendering
   fiveDayHeader.style.display = "";
 
-  
-  let date = (new Date(day.dt*1000).toDateString()); 
+  let date = new Date(day.dt * 1000).toDateString();
   console.log(date);
 
-
   let cardContainer = document.createElement("div");
-  cardContainer.setAttribute('class', "card bg-primary col m-1");
-  cardContainer.style.width = '15rem';
+  cardContainer.setAttribute("class", "card bg-primary col m-1");
+  cardContainer.style.width = "15rem";
   fiveDayEl.append(cardContainer);
 
-
   let cardBody = document.createElement("div");
-  cardBody.setAttribute('class', "card-body");
+  cardBody.setAttribute("class", "card-body");
   cardContainer.append(cardBody);
-
 
   //* Card Title (our future day DATE)
   let cardTitle = document.createElement("h6");
-  cardTitle.setAttribute('class', "card-title ");
+  cardTitle.setAttribute("class", "card-title ");
   cardTitle.textContent = date;
-  cardBody.append(cardTitle);
-  
+  cardContainer.append(cardTitle);
+
   //* Descriptive weather Icon
   //todo GET ICON TO ALWAYS SET AT END OF TITLE(DATE) maybe pseudo? float?
   let cardIcon = document.createElement("img");
-  cardIcon.setAttribute("src", ("http://openweathermap.org/img/wn/" + day.weather[0].icon + ".png"));
-  cardTitle.append(cardIcon);
-
+  cardIcon.setAttribute(
+    "src",
+    "http://openweathermap.org/img/wn/" + day.weather[0].icon + ".png"
+  );
+  cardContainer.append(cardIcon);
 
   //* Temperature readout:
   let cardTemp = document.createElement("p");
-  cardTemp.setAttribute('class', "card-text");
+  cardTemp.setAttribute("class", "card-text");
   cardTemp.textContent = "Temp: " + day.temp.max;
   console.log(day.temp.max);
-  cardBody.append(cardTemp);
+  cardContainer.append(cardTemp);
 
   //* Wind readout:
   let cardWind = document.createElement("p");
-  cardWind.setAttribute('class', "card-text");
+  cardWind.setAttribute("class", "card-text");
   cardWind.textContent = "Wind: " + day.wind_speed + "mph";
-  cardBody.append(cardWind);
+  cardContainer.append(cardWind);
 
   //* Humidity Readout
   let cardHumidity = document.createElement("p");
-  cardHumidity.setAttribute('class', "card-text");
+  cardHumidity.setAttribute("class", "card-text");
   cardHumidity.textContent = "Humidity: " + day.humidity + "%";
-  cardBody.append(cardHumidity);
+  cardContainer.append(cardHumidity);
 
   return cardContainer;
-  cardContainer.append(fiveDayEl)
-};
+  cardContainer.append(fiveDayEl);
+}
 
 init();
 
 //* THE ONE BUTTON TO RULE THEM ALL
-searchBtn.addEventListener("click", function(event) {
+searchBtn.addEventListener("click", function (event) {
   let userCity = userInput.value.trim();
-  
+
   if (!userInput.value) {
-    alert("Please enter a valid city/location"); //todo: change alert to MODAL? 
+    alert("Please enter a valid city/location");
     return;
-  };
+  }
   //* push user's entry into global bucket (array)
   //TODO - create an IF CLAUSE to check entry against Search History
-  searchHistoryBucket.push(userCity);
+
+  //* INCLUDE is a search function (built in)
+  if (searchHistoryBucket.includes(userCity) === false) {
+    searchHistoryBucket.push(userCity);
+  }
   userInput.value = "";
 
   fetchCoords(userCity);
@@ -200,33 +221,29 @@ searchBtn.addEventListener("click", function(event) {
   showHistory();
 });
 
-searchHistEl.addEventListener('click', function(event){
+searchHistEl.addEventListener("click", function (event) {
   let element = event.target;
-  if (element.matches(".history-btn")){
+  if (element.matches(".history-btn")) {
     fetchCoords(element.textContent);
   }
-})
-
+});
 
 //todo BUILD COLOR CODING FUNCTION
 // //write getUVColor function00
-// // //*COLOR CODING FOR 
+// // //*COLOR CODING FOR
 // //   //*  -favorable
 // //   //*   -moderate
 // //   //*    -severe
 // //   //*then return whatever the classs
 
 // function getUVColor () {
-//   if 
-  
-  
+//   if
+
 //   //num is between whatever and etc) {return green;}
 //   //then call function in the weather render to span over the uv index)
 // }
 
-
-//todo variables all set?
-//todo selectors all set?
-
-//todo CHANGE ALERT MESSAGE TO TEXT CONTENT THAT WILL UPDATE UPON EMPTY SEARCH ENTRY AND INVALID LOCATION NAME (BONUS)
-//TODO 
+//TODO: make text smaller for a better fit on the 5-day cards
+//TODO: limit search history results to 5 cities/locations
+//TODO: Change icon size in 5-day
+//todo: darker background for 5-day
